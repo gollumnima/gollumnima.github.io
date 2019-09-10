ec449be2b156bef8800bd7db509b663bdbdf9371
Doori Kim <doori.alice.kim@gmail.com>
2019-09-10 15:08:59 +0900

    add tags in sidebar

--- src/components/Post/Tags/Tags.js
+++ src/components/Post/Tags/Tags.js
@@ -12,7 +12,9 @@ const Tags = ({ tags, tagSlugs }: Props) => (
   <div className={styles['tags']}>
     <ul className={styles['tags__list']}>
       {tagSlugs && tagSlugs.map((slug, i) => (
-        <li className={styles['tags__list-item']} key={tags[i]}>
+        <li className={styles['tags__list-item']}
+        key={tags[i]}
+        >
           <Link to={slug} className={styles['tags__list-item-link']}>
             {tags[i]}
           </Link>
