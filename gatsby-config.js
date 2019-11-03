eec4df24d478b2830cd2e488411cad414a77495b
Doori Kim <doori.alice.kim@gmail.com>
2019-11-03 10:52:36 +0900

    error completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,11 +50,11 @@ module.exports = {
           {
             site {
               siteMetadata {
-                site_url: 'https://gollumnima.github.io/',
-                title: 'Doori's Dev Log',
-                subtitle: '둘둘's Dooreplay!',
-                copyright: '© All rights reserved.',
-                disqusShortname: 'dooreplay' 
+                site_url: "https://gollumnima.github.io/",
+                title: "Doori's Dev Log",
+                subtitle: "둘둘's Dooreplay!",
+                copyright: "© All rights reserved.",
+                disqusShortname: "dooreplay" 
               }
             }
           }
