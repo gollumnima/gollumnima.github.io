ff9b00b930ba85067fc3ff4276b661792a1daaaa
Doori Kim <doori.alice.kim@gmail.com>
2019-12-07 12:53:50 +0900

    야금야금 수정

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ module.exports = {
   url: 'https://gollumnima.github.io/',
   pathPrefix: '/',
   title: '둘둘`s dooreplay!',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  subtitle: 'Doo Reply!',
   copyright: '© All rights reserved.',
-  disqusShortname: '',
-  postsPerPage: 5,
+  disqusShortname: 'dooreplay',
+  postsPerPage: 8,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
@@ -30,11 +30,7 @@ module.exports = {
     bio: '안녕하세요. 말 많은 블로그에 오신걸 환영합니다~ 위코드에서 개발자 꿈나무로 무럭무럭 자라는중입니다!',
     contacts: {
       email: 'doori.alice.kim@gmail.com',
-      telegram: '#',
-      twitter: '#',
-      github: 'gollumnima',
-      rss: '#',
-      vkontakte: '#'
+      github: '/gollumnima',
     }
   }
 };
