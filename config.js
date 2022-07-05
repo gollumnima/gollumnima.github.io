9410ed30fcdc41a0f3686b7761da6a2ad02008e5
Doori Kim <doori.alice.kim@gmail.com>
2022-07-06 00:56:02 +0900

    [Modify] 자기소개 변경

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 module.exports = {
   url: 'https://gollumnima.github.io/',
   pathPrefix: '/',
-  title: '둘둘`s dooreplay!',
+  title: '토공로그!',
   subtitle: 'Doo Reply!',
   copyright: '© All rights reserved.',
   disqusShortname: 'dooreplay',
   postsPerPage: 8,
-  googleAnalyticsId: 'UA-73379983-2',
+  // googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
     {
@@ -25,12 +25,13 @@ module.exports = {
     }
   ],
   author: {
-    name: '둘둘',
+    name: '토공',
     photo: '/photo.jpg',
-    bio: '안녕하세요. 말 많은 블로그에 오신걸 환영합니다~ 위코드에서 개발자 꿈나무로 무럭무럭 자라는중입니다!',
+    bio: '안녕하세요. 저의 임시 블로그입니다~ 블로그가 완선되면 핸드메이드 블로그로 이전할 예정입니다!',
     contacts: {
       email: 'doori.alice.kim@gmail.com',
       github: '/gollumnima',
+      twitter: 'sudokeepcoding'
     }
   }
 };
